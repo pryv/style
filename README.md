af4200056790aa6f6503a53c1a2492be77c50ce2
Simon Goumaz <simon@attentif.ch>
2014-12-15 15:40:55 +0100

    Added setup script

--- README.md
+++ README.md
@@ -11,9 +11,14 @@ Reference CSS, JS and media files.
 
 ### Setup
 
-Install with `npm install`.
+Install with `./dev-env/setup.sh` (which handles cloning of the `gh-pages` branch into `dist` for publishing) or `npm install`.
 Build with `grunt`, which outputs assets to `dist`.
 
+### Publish
+
+**TODO: versioning.**
+
+After building, commit and push the changes from `dist` (i.e. publishes to GitHub pages).
 
 ## License
 
---
+++ dev-env/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This script sets up the Pryv guidelines build environment
+
+# working dir fix
+scriptsFolder=$(cd $(dirname "$0"); pwd)
+cd $scriptsFolder/..
+
+# check for well known prereqs that might be missing
+hash git 2>&- || { echo >&2 "I require git."; exit 1; }
+hash npm 2>&- || { echo >&2 "I require node and npm."; exit 1; }
+
+echo "
+Installing Node modules from 'package.json' if necessary...
+"
+npm install
+
+if [ ! -d build ]
+then
+  echo "
+Setting up 'build' folder for publishing to GitHub pages...
+"
+  git clone -b gh-pages git@github.com:pryv/style.git dist
+fi
+
+echo "
+
+
+If no errors were listed above, the setup is complete.
+See the README for more info on writing and publishing.
+"
